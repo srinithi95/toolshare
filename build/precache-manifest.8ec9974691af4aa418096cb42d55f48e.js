59831e4e08cfe39afee6a33db5e7099aa6a84a33
srinithi95 <60716676+srinithi95@users.noreply.github.com>
2021-02-28 11:19:04 -0800

    testing

---
+++ build/precache-manifest.8ec9974691af4aa418096cb42d55f48e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472af43cfae3ca3e5489e1ed67e3df07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e83f1953deddd78b471",
+    "url": "/static/css/2.67be9a3b.chunk.css"
+  },
+  {
+    "revision": "76ef071dc1cebaa136f4",
+    "url": "/static/css/main.63096cc7.chunk.css"
+  },
+  {
+    "revision": "3e83f1953deddd78b471",
+    "url": "/static/js/2.7106bed5.chunk.js"
+  },
+  {
+    "revision": "76ef071dc1cebaa136f4",
+    "url": "/static/js/main.e59b7611.chunk.js"
+  },
+  {
+    "revision": "2c81a3225bd8c1db4d39",
+    "url": "/static/js/runtime-main.8ba292c2.js"
+  }
+]);
